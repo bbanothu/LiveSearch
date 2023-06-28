85b40c624b764cbfae060742213d517957704d02
Bharath Banothu <bharathbanothu@user.lan>
2023-06-28 12:25:23 -0700

    fixed search from like to from start

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function SearchBar({ searchTerm, handleSearchChange }) {
     return (
         <div class="form-outline mt-2">
-            <input type="search" class="form-control" id="datatable-search-input" value={searchTerm} onChange={handleSearchChange} placeholder="Search by name, age, ssn..." />
+            <input type="search" class="form-control" id="datatable-search-input" value={searchTerm} onChange={handleSearchChange} placeholder="Search by name, age or ssn..." />
         </div>
     );
 }
